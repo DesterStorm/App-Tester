0a5c23fa3a2efa4679e3ec55c21c5eac0a99097f
DesterStorm <daniel.siekawitch@gmail.com>
2020-01-02 11:17:30 -0600

    created edit, new, show and _form views under job_posts.
    made the index, new, create, show, update and edit methods in job_posts_controller.rb
    
    added the "hero" bulma gem template to the application.html.erb layout, creating a create new job posts link and a home page link.

--- app/controllers/job_posts_controller.rb
+++ app/controllers/job_posts_controller.rb
@@ -1,14 +1,44 @@
 class JobPostsController < ApplicationController
 
   def index
-
+    @job_posts = JobPost.all.order("created_at DESC")
   end
 
   def new
-
+    @job_post = JobPost.new
   end
 
   def create
+    @job_post = JobPost.new(job_post_params)
 
+    if @job_post.save
+      redirect_to @job_post
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @job_post = JobPost.find(params[:id])
+  end
+
+  def update
+    @job_post = JobPost.find(params[:id])
+
+    if @job_post.update(job_post_params)
+      redirect_to @job_post
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @job_post = JobPost.find(params[:id])
+  end
+
+  private
+
+  def job_post_params
+    params.require(:job_post).permit(:title, :content)
   end
 end
--- app/models/job_post.rb
+++ app/models/job_post.rb
@@ -1,2 +1,3 @@
 class JobPost < ApplicationRecord
+
 end
